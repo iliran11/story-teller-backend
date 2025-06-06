767dcc2086e9d19b6bb0489e8de5388fc9c760e0
Liran Cohen <iliran11@gmail.com>
2025-06-06 22:33:53 +0300

    Add index page

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const explainerRouter = require("./src/modules/explainer/explainer.route");
 const cors = require("cors");
 
 app.use(morgan('dev'))
-
+app.set('view engine', 'ejs');
+app.set('views', './src/frontend/views');
 app.use(cors());
 app.use("/sub-topics", subtopicsRouter);
 app.use("/topics", topicsRouter);
 app.use("/explainer", explainerRouter);
-app.use("/", (req, res) => {
-  res.send("Hello World");
+
+app.get('/', (req, res) => {
+  res.render('index', {foo: 'FOO'});
 });
 
 app.use((err, req, res, next) => {
